145055ec8c84aef01e73466f8b1ebd7a84ada339
vineboneto <vineboneto@gmail.com>
2021-03-20 18:50:17 -0300

    test: ensure SectorPostgresRepository return true on add succeeds

---
+++ tests/infra/db/postgres-prisma/sector-postgres-repository.spec.ts
@@ -0,0 +1,22 @@
+import { SectorPostgresRepository } from '@/infra/db/postgres-prisma/sector-postgres-repository'
+import { mockAddSectorParams } from '@/tests/domain/mocks/mock-add-sector'
+
+import { PrismaClient } from '@prisma/client'
+
+const prismaClient = new PrismaClient()
+
+describe('SectorPostgresRepository', () => {
+  afterAll(async () => {
+    await prismaClient.$disconnect()
+  })
+
+  beforeEach(async () => {
+    await prismaClient.$executeRaw('DELETE FROM "Sector";')
+  })
+
+  test('Should return true on add succeeds', async () => {
+    const sut = new SectorPostgresRepository(prismaClient)
+    const isValid = await sut.addSector(mockAddSectorParams)
+    expect(isValid).toBeTruthy()
+  })
+})
