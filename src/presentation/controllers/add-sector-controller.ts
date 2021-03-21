5cc96d8e6727a425a0dc1af91f490f311243818c
vineboneto <vineboneto@gmail.com>
2021-03-21 16:56:25 -0300

    feat: return 400 if name not provided

--- src/presentation/controllers/add-sector-controller.ts
+++ src/presentation/controllers/add-sector-controller.ts
@@ -5,13 +5,17 @@ export class AddSectorController implements Controller {
   constructor (private readonly addSector: AddSector) {}
 
   async handle (request: AddSectorController.Request): Promise<HttpResponse> {
+    if (!request.name) {
+      return {
+        statusCode: 400,
+        body: 'Missing param: name'
+      }
+    }
     await this.addSector.add(request)
     return null
   }
 }
 
 export namespace AddSectorController {
-  export type Request = {
-    name: string
-  }
+  export type Request = any
 }
