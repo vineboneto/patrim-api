f0f06b8b9adb57b7045675c6baf1872e12f22676
vineboneto <vineboneto@gmail.com>
2021-03-21 17:22:01 -0300

    feat: ensure AddSectorController return 500 if AddSector throws

--- src/presentation/controllers/add-sector-controller.ts
+++ src/presentation/controllers/add-sector-controller.ts
@@ -1,15 +1,19 @@
 import { AddSector } from '@/domain/usecases'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { MissingParamError } from '@/presentation/errors'
-import { badRequest } from '@/presentation/helper/http-helper'
+import { badRequest, serverError } from '@/presentation/helper/http-helper'
 
 export class AddSectorController implements Controller {
   constructor (private readonly addSector: AddSector) {}
 
   async handle (request: AddSectorController.Request): Promise<HttpResponse> {
-    if (!request.name) return badRequest(new MissingParamError('name'))
-    await this.addSector.add(request)
-    return null
+    try {
+      if (!request.name) return badRequest(new MissingParamError('name'))
+      await this.addSector.add(request)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
--- src/presentation/helper/http-helper.ts
+++ src/presentation/helper/http-helper.ts
@@ -1,6 +1,12 @@
 import { HttpResponse } from '@/presentation/protocols'
+import { ServerError } from '@/presentation/errors'
 
 export const badRequest = (error: Error): HttpResponse => ({
   body: error,
   statusCode: 400
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  body: new ServerError(error.stack),
+  statusCode: 500
 })
