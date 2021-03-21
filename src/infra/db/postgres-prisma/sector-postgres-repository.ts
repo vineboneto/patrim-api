f8b7365ab4f8d51459e22dbe32e90025b1f9ff0e
vineboneto <vineboneto@gmail.com>
2021-03-21 14:48:11 -0300

    feat: ensure SectorPostgresRepository return true on add succeeds

--- src/infra/db/postgres-prisma/sector-postgres-repository.ts
+++ src/infra/db/postgres-prisma/sector-postgres-repository.ts
@@ -1,10 +1,10 @@
 import { AddSectorRepository } from '@/data/protocols'
-import { PrismaHelper } from '@/infra/db/postgres-prisma/prima-helper'
+import { PrismaHelper } from '@/infra/db/postgres-prisma/prisma-helper'
 
 export class SectorPostgresRepository implements AddSectorRepository {
   async addSector (sector: AddSectorRepository.Params): Promise<AddSectorRepository.Result> {
     const { name } = sector
-    const prismaClient = await PrismaHelper.getCollection()
+    const prismaClient = await PrismaHelper.getConnection()
     const sectorResult = await prismaClient.sector.create({
       data: {
         name
