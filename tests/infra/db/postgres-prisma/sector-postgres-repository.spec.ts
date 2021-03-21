71a5bc12dbfe42461a8ceb19234baa400553f470
vineboneto <vineboneto@gmail.com>
2021-03-21 15:35:22 -0300

    test: ensure return true if sector name exists

--- tests/infra/db/postgres-prisma/sector-postgres-repository.spec.ts
+++ tests/infra/db/postgres-prisma/sector-postgres-repository.spec.ts
@@ -1,24 +1,43 @@
 import { SectorPostgresRepository, PrismaHelper } from '@/infra/db/postgres-prisma'
 import { mockAddSectorParams } from '@/tests/domain/mocks/mock-add-sector'
 
+let prismaClient
+
 describe('SectorPostgresRepository', () => {
   beforeAll(() => {
     PrismaHelper.connect()
   })
 
   afterAll(async () => {
+    await prismaClient.$executeRaw('DELETE FROM "Sector";')
     PrismaHelper.disconnect()
   })
 
   beforeEach(async () => {
-    const prismaClient = await PrismaHelper.getConnection()
+    prismaClient = await PrismaHelper.getConnection()
     await prismaClient.$executeRaw('ALTER SEQUENCE "Sector_id_seq" RESTART WITH 1;')
     await prismaClient.$executeRaw('DELETE FROM "Sector";')
   })
 
-  test('Should return true on add succeeds', async () => {
-    const sut = new SectorPostgresRepository()
-    const isValid = await sut.addSector(mockAddSectorParams())
-    expect(isValid).toBeTruthy()
+  describe('addSector()', () => {
+    test('Should return true on add succeeds', async () => {
+      const sut = new SectorPostgresRepository()
+      const isValid = await sut.addSector(mockAddSectorParams())
+      expect(isValid).toBeTruthy()
+    })
+  })
+
+  describe('checkByName()', () => {
+    test('Should return true if sector name exists', async () => {
+      const sut = new SectorPostgresRepository()
+      const { name } = mockAddSectorParams()
+      await prismaClient.sector.create({
+        data: {
+          name
+        }
+      })
+      const isValid = await sut.checkByName(name)
+      expect(isValid).toBe(true)
+    })
   })
 })
