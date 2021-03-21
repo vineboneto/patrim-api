c3843c850f15803558d937d1916930a3c89221b0
vineboneto <vineboneto@gmail.com>
2021-03-21 17:22:37 -0300

    test: ensure AddSectorController return 500 if AddSector throws

--- tests/presentation/controllers/add-sector-controller.spec.ts
+++ tests/presentation/controllers/add-sector-controller.spec.ts
@@ -1,7 +1,7 @@
 import { AddSectorController } from '@/presentation/controllers'
 import { AddSectorSpy } from '@/tests/presentation/mocks'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
-import { badRequest } from '@/presentation/helper/http-helper'
+import { badRequest, serverError } from '@/presentation/helper/http-helper'
 
 import faker from 'faker'
 
@@ -35,5 +35,12 @@ describe('AddSectorController', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
+  })
+
+  test('Should return 500 if AddSector throws', async () => {
+    const { sut, addSectorSpy } = makeSut()
+    jest.spyOn(addSectorSpy, 'add').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
