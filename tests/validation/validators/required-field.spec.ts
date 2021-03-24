31c3713b1ec06ba56677c0c297cdccd495b8bbdf
vineboneto <vineboneto@gmail.com>
2021-03-23 21:50:33 -0300

    refactor: move SUT creation to a factory method

--- tests/validation/validators/required-field.spec.ts
+++ tests/validation/validators/required-field.spec.ts
@@ -3,16 +3,20 @@ import { RequiredField } from '@/validation/validators'
 
 import faker from 'faker'
 
+const makeSut = (fieldName: string): RequiredField => new RequiredField(fieldName)
+
 describe('RequiredField', () => {
   test('Should not return if field exists', () => {
-    const sut = new RequiredField('field')
-    const error = sut.validate({ field: faker.random.word() })
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const error = sut.validate({ [field]: faker.random.word() })
     expect(error).toBeFalsy()
   })
 
   test('Should return MissingParamError if field does not exits', () => {
-    const sut = new RequiredField('field')
-    const error = sut.validate({ otherField: 'any_value' })
-    expect(error).toEqual(new MissingParamError('field'))
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const error = sut.validate({ otherField: faker.random.word() })
+    expect(error).toEqual(new MissingParamError(field))
   })
 })
