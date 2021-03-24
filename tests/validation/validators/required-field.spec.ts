61722e1210f974900e6e24d5634932dc6e943002
vineboneto <vineboneto@gmail.com>
2021-03-23 21:34:30 -0300

    test: ensure RequiredField not return if field exists

--- tests/validation/validators/required-field.spec.ts
+++ tests/validation/validators/required-field.spec.ts
@@ -1,11 +1,18 @@
+import { MissingParamError } from '@/presentation/errors'
 import { RequiredField } from '@/validation/validators'
 
 import faker from 'faker'
 
 describe('RequiredField', () => {
   test('Should not return if field exists', () => {
-    const sut = new RequiredField()
+    const sut = new RequiredField('field')
     const error = sut.validate({ field: faker.random.word() })
     expect(error).toBeFalsy()
+  })
+
+  test('Should return MissingParamError if field does not exits', () => {
+    const sut = new RequiredField('field')
+    const error = sut.validate({ otherField: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
   })
 })
