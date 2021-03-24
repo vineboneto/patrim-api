1fa945b711a2127f1ff9573a296fca6fa8f71377
vineboneto <vineboneto@gmail.com>
2021-03-23 21:33:55 -0300

    feat: ensure RequiredField not return if field exists

--- src/validation/validators/required-field.ts
+++ src/validation/validators/required-field.ts
@@ -1,7 +1,11 @@
+import { MissingParamError } from '@/presentation/errors'
 import { Validation } from '@/presentation/protocols/validation'
 
 export class RequiredField implements Validation {
+  constructor (private readonly fieldName: string) {}
+
   validate (input: any): Error {
+    if (!input[this.fieldName]) return new MissingParamError(this.fieldName)
     return null
   }
 }
