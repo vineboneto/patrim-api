8d68d6fbcd23c7753f1dded05a7c846026d9aaee
vineboneto <vineboneto@gmail.com>
2021-03-24 20:21:24 -0300

    feat: ensure AddSectorController calls Validation with correct value

--- src/presentation/controllers/add-sector-controller.ts
+++ src/presentation/controllers/add-sector-controller.ts
@@ -1,13 +1,18 @@
 import { AddSector } from '@/domain/usecases'
-import { Controller, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest, noContent, serverError } from '@/presentation/helper/http-helper'
 
 export class AddSectorController implements Controller {
-  constructor (private readonly addSector: AddSector) {}
+  constructor (
+    private readonly addSector: AddSector,
+    private readonly validation: Validation
+  ) {}
 
   async handle (request: AddSectorController.Request): Promise<HttpResponse> {
     try {
+      this.validation.validate(request)
+
       if (!request.name) return badRequest(new MissingParamError('name'))
       await this.addSector.add(request)
       return noContent()
