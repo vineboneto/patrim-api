aefdedd8515d1eade9a87cfdfc388002ac03c154
vineboneto <vineboneto@gmail.com>
2021-03-28 16:10:39 -0300

    feat: add use case addAccount

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,12 @@
+export interface AddAccount {
+  add (AddAccount: AddAccount.Params): Promise<AddAccount.Result>
+}
+
+export namespace AddAccount {
+  export type Params = {
+    name: string
+    email: string
+    password: string
+  }
+  export type Result = boolean
+}
