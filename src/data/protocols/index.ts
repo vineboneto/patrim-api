9f68e8002bb08c8126649fe5f4ea3355e0620635
vineboneto <vineboneto@gmail.com>
2021-03-28 17:06:58 -0300

    feat: ensure DbAddAccount call CheckAccountByEmailRepository with correct values

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -1,3 +1,4 @@
 export * from './check-sector-by-name-repository'
+export * from './check-account-by-email-repository'
 export * from './add-sector-repository'
 export * from './add-account-repository'
--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,10 +1,14 @@
 import { AddAccount } from '@/domain/usecases'
-import { AddAccountRepository } from '@/data/protocols'
+import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly addAccountRepository: AddAccountRepository) {}
+  constructor (
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
+  ) {}
 
   async add (account: AddAccount.Params): Promise<AddAccount.Result> {
+    await this.checkAccountByEmailRepository.checkByEmail(account.email)
     const isValid = await this.addAccountRepository.add(account)
     return isValid
   }
