00b586962611ae9a0865d405d0b532387ab68477
Ivo Melo <ivo.melo.988@gmail.com>
2021-03-28 17:30:43 -0300

    feat: ensure AddCategoryRepositories is called with correct values

--- src/data/usecases/db-add-category.ts
+++ src/data/usecases/db-add-category.ts
@@ -1,7 +1,11 @@
 import { AddCategory } from '@/domain/usecases/add-category'
+import { AddCategoryRepository } from '@/data/protocols/add-category-repository'
 
 export class DbAddCategory implements AddCategory {
+  constructor (private readonly addCategoryRepository: AddCategoryRepository) {}
+
   async add (category: AddCategory.Params): Promise<AddCategory.Result> {
-    return true
+    await this.addCategoryRepository.addCategory(category)
+    return null
   }
 }
