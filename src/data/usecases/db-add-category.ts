e9d898a9cdb78663c9a5dd48312bacf56e077c61
Ivo Melo <ivo.melo.988@gmail.com>
2021-03-28 20:13:30 -0300

    feat: ensure DbAddCategory should return false if CheckCategoryByNameRepository return true

--- src/data/usecases/db-add-category.ts
+++ src/data/usecases/db-add-category.ts
@@ -5,8 +5,11 @@ export class DbAddCategory implements AddCategory {
   constructor (private readonly addCategoryRepository: AddCategoryRepository, private readonly checkCategoryByNameRepository: CheckCategoryByNameRepository) {}
 
   async add (category: AddCategory.Params): Promise<AddCategory.Result> {
-    await this.checkCategoryByNameRepository.checkByName(category.name)
-    const isValid = await this.addCategoryRepository.addCategory(category)
-    return isValid
+    const exists = await this.checkCategoryByNameRepository.checkByName(category.name)
+    if (exists) return false
+    else {
+      const isValid = await this.addCategoryRepository.addCategory(category)
+      return isValid
+    }
   }
 }
