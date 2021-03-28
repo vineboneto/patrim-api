d34adf7d62eb5fe6924336ff1f2737c366fda9a4
Ivo Melo <ivo.melo.988@gmail.com>
2021-03-28 19:51:04 -0300

    feat: ensure CheckCategoryByNameRepositories is called with correct values

--- src/data/usecases/db-add-category.ts
+++ src/data/usecases/db-add-category.ts
@@ -1,10 +1,11 @@
 import { AddCategory } from '@/domain/usecases/'
-import { AddCategoryRepository } from '@/data/protocols/'
+import { AddCategoryRepository, CheckCategoryByNameRepository } from '@/data/protocols/'
 
 export class DbAddCategory implements AddCategory {
-  constructor (private readonly addCategoryRepository: AddCategoryRepository) {}
+  constructor (private readonly addCategoryRepository: AddCategoryRepository, private readonly checkCategoryByNameRepository: CheckCategoryByNameRepository) {}
 
   async add (category: AddCategory.Params): Promise<AddCategory.Result> {
+    await this.checkCategoryByNameRepository.checkByName(category.name)
     const isValid = await this.addCategoryRepository.addCategory(category)
     return isValid
   }
