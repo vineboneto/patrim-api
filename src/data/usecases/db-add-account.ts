20c88154eab1b6439c27b2d6eecae8b4c677b5e2
vineboneto <vineboneto@gmail.com>
2021-03-28 17:28:16 -0300

    feat: ensure DbAddAccount calls Hasher with correct values

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,16 +1,18 @@
 import { AddAccount } from '@/domain/usecases'
-import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols'
+import { AddAccountRepository, CheckAccountByEmailRepository, Hasher } from '@/data/protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly addAccountRepository: AddAccountRepository,
-    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository,
+    private readonly hasher: Hasher
   ) {}
 
   async add (account: AddAccount.Params): Promise<AddAccount.Result> {
     const exists = await this.checkAccountByEmailRepository.checkByEmail(account.email)
     let isValid = false
     if (!exists) {
+      await this.hasher.hash(account.password)
       isValid = await this.addAccountRepository.add(account)
     }
     return isValid
