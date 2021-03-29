d33116a402bdc6cc90254417cb84dddd598679a4
vineboneto <vineboneto@gmail.com>
2021-03-29 00:37:24 -0300

    refactor: organize imports

---
+++ src/data/protocols/db/index.ts
@@ -0,0 +1,5 @@
+export * from './load-account-by-email-repository'
+export * from './check-sector-by-name-repository'
+export * from './check-account-by-email-repository'
+export * from './add-sector-repository'
+export * from './add-account-repository'
