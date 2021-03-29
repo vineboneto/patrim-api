fb3258407b5f683485c842b167fb0109256b8b42
vineboneto <vineboneto@gmail.com>
2021-03-29 20:54:17 -0300

    test: ensure JwtAdapter calls sign with correct values

---
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -0,0 +1,18 @@
+import { JwtAdapter } from '@/infra/criptography'
+
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return Promise.resolve('any_token')
+  }
+}))
+
+describe('JwtAdapter', () => {
+  test('Should call sign with correct values', async () => {
+    const sut = new JwtAdapter('secret_key')
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret_key')
+  })
+})
