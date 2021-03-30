bd9c3be1a7b1f0296f85e7f57f9a23f32361f2fa
vineboneto <vineboneto@gmail.com>
2021-03-30 00:13:11 -0300

    test: ensure /api/signup return 200 on signup

--- tests/main/routes/login-routes.spec.ts
+++ tests/main/routes/login-routes.spec.ts
@@ -19,8 +19,8 @@ describe('Login Routes', () => {
 
     beforeEach(async () => {
       prismaClient = await PrismaHelper.getConnection()
-      await prismaClient.$executeRaw('ALTER SEQUENCE "Sector_id_seq" RESTART WITH 1;')
-      await prismaClient.$executeRaw('DELETE FROM "Sector";')
+      await prismaClient.$executeRaw('ALTER SEQUENCE "User_id_seq" RESTART WITH 1;')
+      await prismaClient.$executeRaw('DELETE FROM "User";')
     })
 
     test('Should return badRequest if same required data not provided', async () => {
@@ -33,6 +33,19 @@ describe('Login Routes', () => {
           passwordConfirmation: password
         })
         .expect(400)
+    })
+
+    test('Should return 200 on signup', async () => {
+      const password = faker.internet.password()
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: faker.name.findName(),
+          email: faker.internet.email(),
+          password,
+          passwordConfirmation: password
+        })
+        .expect(200)
     })
   })
 })
