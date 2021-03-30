2c465591a31815aabec517ddcba946e79e9f7be4
vineboneto <vineboneto@gmail.com>
2021-03-29 21:14:59 -0300

    test: ensure JwtAdapter throw if sign throws

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -21,4 +21,11 @@ describe('JwtAdapter', () => {
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret_key')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
