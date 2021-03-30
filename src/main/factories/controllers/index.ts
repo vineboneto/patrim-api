a339df088d8e29d2b26bf9fb5ff0bfaa8cde57f3
vineboneto <vineboneto@gmail.com>
2021-03-29 23:50:35 -0300

    feat: create factory method to SignUpController

--- src/main/factories/controllers/index.ts
+++ src/main/factories/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from './add-sector-controller-factory'
 export * from './add-sector-validation-factory'
+export * from './signup-controller-factory'
 export * from './signup-validation-factory'
