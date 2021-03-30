33d03342bfb2f582cebcf6e43ab19b96aae475e5
vineboneto <vineboneto@gmail.com>
2021-03-29 22:47:24 -0300

    test: ensure CompareFieldsValidation return InvalidParamError if validation fails

---
+++ tests/validation/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,13 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { CompareFieldsValidation } from '@/validation/validators'
+
+describe('CompareFieldValidation', () => {
+  test('Should return InvalidParamError if validation fails', () => {
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const isValid = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'other_value'
+    })
+    expect(isValid).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+})
