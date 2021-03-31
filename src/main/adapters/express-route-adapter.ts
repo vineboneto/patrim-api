fcbde725285d833705635a2c5f0598e1ebed724f
vineboneto <vineboneto@gmail.com>
2021-03-31 12:44:27 -0300

    feat: create Middleware adapter

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -4,7 +4,9 @@ import { Request, Response } from 'express'
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const request = {
-      ...(req.body || {})
+      ...(req.body || {}),
+      ...(req.params || {}),
+      accountId: req.accountId
     }
     const httpResponse = await controller.handle(request)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
