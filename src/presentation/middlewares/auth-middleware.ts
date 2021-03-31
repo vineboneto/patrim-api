f1e776cfd220e216cd28f2438f7f46c57dcd4473
vineboneto <vineboneto@gmail.com>
2021-03-31 12:13:56 -0300

    feat: ensure AuthMiddleware call LoadAccountByToken with correct values

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,20 @@
+import { HttpResponse, Middleware } from '@/presentation/protocols'
+import { LoadAccountByToken } from '@/domain/usecases'
+
+export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly role?: string
+  ) {}
+
+  async handle (request: AuthMiddleware.Request): Promise<HttpResponse> {
+    const { accessToken } = request
+    await this.loadAccountByToken.load(accessToken, this.role)
+  }
+}
+
+export namespace AuthMiddleware {
+  export type Request = {
+    accessToken: string
+  }
+}
