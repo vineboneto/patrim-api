4329679e2fe4a5e36490125a3b99895d1dded57f
Ivo Melo <ivo.melo.988@gmail.com>
2021-03-30 22:31:01 -0300

    feat: ensure AddCategoryController calls Validation with correct values

--- src/presentation/controllers/add-category-controller.ts
+++ src/presentation/controllers/add-category-controller.ts
@@ -1,10 +1,14 @@
 import { AddCategory } from '@/domain/usecases'
-import { Controller, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class AddCategoryController implements Controller {
-  constructor (private readonly addCategory: AddCategory) {}
+  constructor (
+    private readonly addCategory: AddCategory,
+    private readonly validation: Validation
+  ) {}
 
   async handle (request: AddCategoryController.Request): Promise<HttpResponse> {
+    this.validation.validate(request)
     await this.addCategory.add(request)
     return null
   }
