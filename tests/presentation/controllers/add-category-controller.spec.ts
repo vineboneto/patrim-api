18afef86f54c17e2f4a73453254ba74578adb0af
Ivo Melo <ivo.melo.988@gmail.com>
2021-03-30 22:32:02 -0300

    test: add test AddCAtegoryController calls Validation with correct values

--- tests/presentation/controllers/add-category-controller.spec.ts
+++ tests/presentation/controllers/add-category-controller.spec.ts
@@ -1,13 +1,36 @@
 import { AddCategoryController } from '@/presentation/controllers/'
-import { AddCategorySpy } from '@/tests/presentation/mocks'
+import { AddCategorySpy, ValidationSpy } from '@/tests/presentation/mocks'
 import faker from 'faker'
+
+type SutTypes = {
+  addCategorySpy: AddCategorySpy
+  validationSpy: ValidationSpy
+  sut: AddCategoryController
+}
+
+const makeSut = (): SutTypes => {
+  const addCategorySpy = new AddCategorySpy()
+  const validationSpy = new ValidationSpy()
+  const sut = new AddCategoryController(addCategorySpy, validationSpy)
+  return {
+    addCategorySpy,
+    validationSpy,
+    sut
+  }
+}
 
 describe('AddCategoryController', () => {
   test('Should call AddSector with correct values', async () => {
-    const addCategorySpy = new AddCategorySpy()
-    const sut = new AddCategoryController(addCategorySpy)
+    const { sut, addCategorySpy } = makeSut()
     const request = { name: faker.name.jobTitle() }
     await sut.handle(request)
     expect(addCategorySpy.params).toEqual(request)
+  })
+
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationSpy } = makeSut()
+    const request = { name: faker.name.jobTitle() }
+    await sut.handle(request)
+    expect(validationSpy.input).toEqual(request)
   })
 })
