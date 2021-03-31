cb94a5104c6c062a9422f2dd6454435bd818bbb5
vineboneto <vineboneto@gmail.com>
2021-03-31 11:01:47 -0300

    feat: ensure DbLoadAccountByToken return null if LoadAccountByTokenRepository returns null

--- src/data/usecases/db-load-account-by-token.ts
+++ src/data/usecases/db-load-account-by-token.ts
@@ -5,7 +5,7 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository) {}
 
   async load (accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
-    await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
-    return null
+    const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+    return account
   }
 }
