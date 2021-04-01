623120e9be13c3984548427e770d54d3fe892aeb
Ivo Melo <ivo.melo.988@gmail.com>
2021-03-31 21:29:25 -0300

    test: add test Should return 204 on success

--- tests/presentation/controllers/add-category-controller.spec.ts
+++ tests/presentation/controllers/add-category-controller.spec.ts
@@ -1,7 +1,7 @@
 import { AddCategoryController } from '@/presentation/controllers/'
 import { AddCategorySpy, ValidationSpy } from '@/tests/presentation/mocks'
 import faker from 'faker'
-import { badRequest, forbidden, noContent } from '@/presentation/helper'
+import { badRequest, forbidden, noContent, serverError } from '@/presentation/helper'
 import { AlreadyExistsError } from '@/presentation/errors'
 
 type SutTypes = {
@@ -49,6 +49,13 @@ describe('AddCategoryController', async () => {
     const param = { name: faker.name.jobArea() }
     const exists = await sut.handle(param)
     expect(exists).toEqual(forbidden(new AlreadyExistsError(param.name)))
+  })
+
+  test('Should return 500 if AddCategory throws', async () => {
+    const { sut, addCategorySpy } = makeSut()
+    jest.spyOn(addCategorySpy, 'add').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle({ name: faker.name.jobArea() })
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 
   test('Should return 204 on success', async () => {
