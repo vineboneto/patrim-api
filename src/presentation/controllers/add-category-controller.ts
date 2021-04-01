9a63da5a7e3a45bf4def96ffa753ca7d64262503
Ivo Melo <ivo.melo.988@gmail.com>
2021-03-31 21:28:37 -0300

    feat: ensure AddCategoryController returns 500 if AddCategory throws

--- src/presentation/controllers/add-category-controller.ts
+++ src/presentation/controllers/add-category-controller.ts
@@ -1,6 +1,6 @@
 import { AddCategory } from '@/domain/usecases'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
-import { badRequest, forbidden, noContent } from '@/presentation/helper/'
+import { badRequest, forbidden, noContent, serverError } from '@/presentation/helper/'
 import { AlreadyExistsError } from '@/presentation/errors'
 
 export class AddCategoryController implements Controller {
@@ -10,10 +10,14 @@ export class AddCategoryController implements Controller {
   ) {}
 
   async handle (request: AddCategoryController.Request): Promise<HttpResponse> {
-    const error = this.validation.validate(request)
-    if (error) return badRequest(error)
-    if (!await this.addCategory.add(request)) return forbidden(new AlreadyExistsError(request.name))
-    return noContent()
+    try {
+      const error = this.validation.validate(request)
+      if (error) return badRequest(error)
+      if (!await this.addCategory.add(request)) return forbidden(new AlreadyExistsError(request.name))
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
