62fbaaa75854ea0e2a69ad9e023a323d8eb8ec51
vineboneto <vineboneto@gmail.com>
2021-04-01 10:36:15 -0300

    test: ensure AddCategoryValidation call ValidationComposite with all validations

---
+++ tests/main/factories/controllers/add-category-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { makeAddCategoryValidation } from '@/main/factories/controllers'
+import { ValidationComposite, RequiredFieldValidation } from '@/validation/validators'
+import { Validation } from '@/presentation/protocols'
+
+jest.mock('@/validation/validators/validation-composite')
+
+describe('AddCategoryValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeAddCategoryValidation()
+    const validations: Validation[] = []
+    for (const field of ['name']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
