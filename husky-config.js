4f7b9f73d35fe4ee433187baedb0ea258dfa6004
vineboneto <vineboneto@gmail.com>
2021-04-07 21:29:19 -0300

    chore: update husky config

--- husky-config.js
+++ husky-config.js
@@ -5,7 +5,7 @@ const scripts = [
   'npx rimraf .config',
   'npx husky install .config/husky',
   'npx husky add .config/husky/pre-push "yarn test:ci"',
-  'npx husky add .config/husky/pre-commit "yarn lint-staged && yarn test:staged"',
+  'npx husky add .config/husky/pre-commit "yarn lint-staged"',
   'npx husky add .config/husky/commit-msg ".git/hooks/commit-msg $1"'
 ]
 
