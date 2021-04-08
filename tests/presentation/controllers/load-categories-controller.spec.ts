c8d1ec08ebdb1b5e88dd9f8073830d7e8ad52964
vineboneto <vineboneto@gmail.com>
2021-04-08 11:41:03 -0300

    test: ensure LoadCategoriesController return 500 if LoadCategories throws'

--- tests/presentation/controllers/load-categories-controller.spec.ts
+++ tests/presentation/controllers/load-categories-controller.spec.ts
@@ -1,4 +1,5 @@
 import { LoadCategoriesController } from '@/presentation/controllers'
+import { serverError } from '@/presentation/helper'
 import { LoadCategoriesSpy } from '@/tests/domain/mocks'
 
 type SutTypes = {
@@ -20,5 +21,13 @@ describe('LoadCategoriesController', () => {
     const { sut, loadCategoriesSpy } = makeSut()
     await sut.handle()
     expect(loadCategoriesSpy.callsCount).toBe(1)
+  })
+
+  test('Should return 500 if LoadCategories throws', async () => {
+    const { sut, loadCategoriesSpy } = makeSut()
+    const error = new Error()
+    jest.spyOn(loadCategoriesSpy, 'load').mockRejectedValueOnce(error)
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(serverError(error))
   })
 })
