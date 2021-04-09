521ba06fd2f9d21a015a5fe9e9877b3a62e5704d
vineboneto <vineboneto@gmail.com>
2021-04-09 11:03:44 -0300

    feat: ensure DeleteCategoryController call Validation with correct value

--- src/presentation/controllers/category/delete-category-controller.ts
+++ src/presentation/controllers/category/delete-category-controller.ts
@@ -1,12 +1,14 @@
-import { Controller, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { DeleteCategory } from '@/domain/usecases'
 
 export class DeleteCategoryController implements Controller {
   constructor (
-    private readonly deleteCategory: DeleteCategory
+    private readonly deleteCategory: DeleteCategory,
+    private readonly validation: Validation
   ) {}
 
   async handle (request: DeleteCategoryController.Request): Promise<HttpResponse> {
+    this.validation.validate(request)
     await this.deleteCategory.delete({ id: Number(request.id) })
     return null
   }
