299f80223878b905a39b9248167254acb9c1623f
vineboneto <vineboneto@gmail.com>
2021-04-09 03:53:32 -0300

    feat: ensure DeleteSectorValidationFactory call ValidationComposite with all validations

--- src/main/factories/controllers/sector/index.ts
+++ src/main/factories/controllers/sector/index.ts
@@ -1,3 +1,4 @@
 export * from './add-sector-controller-factory'
 export * from './add-sector-validation-factory'
 export * from './load-sectors-controller-factory'
+export * from './delete-sector-validation-factory'
