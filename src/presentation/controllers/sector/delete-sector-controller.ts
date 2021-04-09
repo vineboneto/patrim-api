fbc19e9869f6fa82c72684b200d7cad4b0d18a51
vineboneto <vineboneto@gmail.com>
2021-04-09 03:09:13 -0300

    feat: ensure DeleteSectorController calls Validation with correct values

--- src/presentation/controllers/sector/delete-sector-controller.ts
+++ src/presentation/controllers/sector/delete-sector-controller.ts
@@ -1,12 +1,14 @@
-import { Controller, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { DeleteSector } from '@/domain/usecases'
 
 export class DeleteSectorController implements Controller {
   constructor (
-    private readonly deleteSector: DeleteSector
+    private readonly deleteSector: DeleteSector,
+    private readonly validationSpy: Validation
   ) {}
 
   async handle (request: DeleteSectorController.Request): Promise<HttpResponse> {
+    await this.validationSpy.validate(request)
     await this.deleteSector.delete({ id: Number(request.id) })
     return null
   }
