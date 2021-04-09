5e4703aba9a3c315a8cdfc38b10dd2557ded4f53
vineboneto <vineboneto@gmail.com>
2021-04-09 11:16:24 -0300

    feat: ensure DeleteCategoryValidation call ValidationComposite with all validations

--- src/main/factories/controllers/category/index.ts
+++ src/main/factories/controllers/category/index.ts
@@ -1,3 +1,4 @@
 export * from './add-category-controller-factory'
 export * from './add-category-validation-factory'
+export * from './delete-category-validation-factory'
 export * from './load-categories-controller-factory'
