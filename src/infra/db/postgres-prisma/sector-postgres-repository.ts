5ba6bdc996577000b413c9311e187535b7c82598
vineboneto <vineboneto@gmail.com>
2021-04-09 02:41:22 -0300

    feat: ensure SectorPostgresRepository return sector on delete success

--- src/infra/db/postgres-prisma/sector-postgres-repository.ts
+++ src/infra/db/postgres-prisma/sector-postgres-repository.ts
@@ -2,15 +2,17 @@ import {
   AddSectorRepository,
   CheckSectorByIdRepository,
   CheckSectorByNameRepository,
+  DeleteSectorRepository,
   LoadSectorsRepository
 } from '@/data/protocols'
 import { PrismaHelper } from '@/infra/db/postgres-prisma'
 
 export class SectorPostgresRepository implements
   AddSectorRepository,
+  DeleteSectorRepository,
   CheckSectorByNameRepository,
-  LoadSectorsRepository,
-  CheckSectorByIdRepository {
+  CheckSectorByIdRepository,
+  LoadSectorsRepository {
   async addSector (sector: AddSectorRepository.Params): Promise<AddSectorRepository.Model> {
     const { name } = sector
     const prismaClient = await PrismaHelper.getConnection()
@@ -22,20 +24,27 @@ export class SectorPostgresRepository implements
     return sectorModel !== null
   }
 
+  async delete (id: number): Promise<DeleteSectorRepository.Model> {
+    const prismaClient = await PrismaHelper.getConnection()
+    const sectorDeleted = await prismaClient.sector.delete({
+      where: {
+        id: id
+      }
+    })
+    return sectorDeleted
+  }
+
   async checkByName (name: string): Promise<boolean> {
     const prismaClient = await PrismaHelper.getConnection()
     const sector = await prismaClient.sector.findFirst({
       where: {
         name: name
+      },
+      select: {
+        id: true
       }
     })
     return sector !== null
-  }
-
-  async loadAll (): Promise<LoadSectorsRepository.Model> {
-    const prismaClient = await PrismaHelper.getConnection()
-    const sectors = await prismaClient.sector.findMany()
-    return sectors
   }
 
   async checkById (id: number): Promise<CheckSectorByIdRepository.Result> {
@@ -49,5 +58,11 @@ export class SectorPostgresRepository implements
       }
     })
     return sectorWithOnlyId !== null
+  }
+
+  async loadAll (): Promise<LoadSectorsRepository.Model> {
+    const prismaClient = await PrismaHelper.getConnection()
+    const sectors = await prismaClient.sector.findMany()
+    return sectors
   }
 }
