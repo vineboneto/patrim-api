cf6f9dd59355a0d9ad28cb6c80b2297902f0a0e5
vineboneto <vineboneto@gmail.com>
2021-04-09 10:35:25 -0300

    test: ensure DbDeleteCategory call CheckCategoryByIdRepository with correct values

--- tests/data/usecases/category/db-delete-category.spec.ts
+++ tests/data/usecases/category/db-delete-category.spec.ts
@@ -1,18 +1,21 @@
 import { DbDeleteCategory } from '@/data/usecases'
-import { DeleteCategoryRepositorySpy } from '@/tests/data/mocks'
+import { DeleteCategoryRepositorySpy, CheckCategoryByIdRepositorySpy } from '@/tests/data/mocks'
 import { mockDeleteCategoryParams } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: DbDeleteCategory
   deleteCategoryRepositorySpy: DeleteCategoryRepositorySpy
+  checkCategoryByIdRepositorySpy: CheckCategoryByIdRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
+  const checkCategoryByIdRepositorySpy = new CheckCategoryByIdRepositorySpy()
   const deleteCategoryRepositorySpy = new DeleteCategoryRepositorySpy()
-  const sut = new DbDeleteCategory(deleteCategoryRepositorySpy)
+  const sut = new DbDeleteCategory(deleteCategoryRepositorySpy, checkCategoryByIdRepositorySpy)
   return {
     sut,
-    deleteCategoryRepositorySpy
+    deleteCategoryRepositorySpy,
+    checkCategoryByIdRepositorySpy
   }
 }
 
@@ -22,5 +25,12 @@ describe('DbDeleteCategory', () => {
     const { id } = mockDeleteCategoryParams()
     await sut.delete({ id })
     expect(deleteCategoryRepositorySpy.id).toBe(id)
+  })
+
+  test('Should call CheckCategoryByIdRepository with correct values', async () => {
+    const { sut, checkCategoryByIdRepositorySpy } = makeSut()
+    const { id } = mockDeleteCategoryParams()
+    await sut.delete({ id })
+    expect(checkCategoryByIdRepositorySpy.id).toBe(id)
   })
 })
