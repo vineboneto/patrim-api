e62e4425389272b3d86c86c0cd6fce6e31c4fc5b
vineboneto <vineboneto@gmail.com>
2021-04-10 14:43:53 -0300

    refactor: move SUT creation to a factory method

--- tests/data/usecases/category/db-check-category-by-id.spec.ts
+++ tests/data/usecases/category/db-check-category-by-id.spec.ts
@@ -1,12 +1,25 @@
 import { DbCheckByIdCategory } from '@/data/usecases'
-import { CheckSectorByIdRepositorySpy } from '@/tests/data/mocks'
+import { CheckCategoryByIdRepositorySpy } from '@/tests/data/mocks'
 
 import faker from 'faker'
 
+type SutTypes = {
+  sut: DbCheckByIdCategory
+  checkSectorByIdRepositorySpy: CheckCategoryByIdRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const checkSectorByIdRepositorySpy = new CheckCategoryByIdRepositorySpy()
+  const sut = new DbCheckByIdCategory(checkSectorByIdRepositorySpy)
+  return {
+    sut,
+    checkSectorByIdRepositorySpy
+  }
+}
+
 describe('DbCheckCategoryById', () => {
   test('Should call CheckCategoryById with correct value', async () => {
-    const checkSectorByIdRepositorySpy = new CheckSectorByIdRepositorySpy()
-    const sut = new DbCheckByIdCategory(checkSectorByIdRepositorySpy)
+    const { sut, checkSectorByIdRepositorySpy } = makeSut()
     const id = faker.datatype.number()
     await sut.checkById(id)
     expect(checkSectorByIdRepositorySpy.id).toBe(id)
