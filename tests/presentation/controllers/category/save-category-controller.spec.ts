199040f7402edbe7d7cad7e5193ec03f53c775dd
vineboneto <vineboneto@gmail.com>
2021-04-10 15:07:07 -0300

    test: ensure SaveCategoryController return 400 Validation if validation fails

--- tests/presentation/controllers/category/save-category-controller.spec.ts
+++ tests/presentation/controllers/category/save-category-controller.spec.ts
@@ -1,4 +1,6 @@
 import { SaveCategoryController } from '@/presentation/controllers'
+import { badRequest } from '@/presentation/helper'
+import { MissingParamError } from '@/presentation/errors'
 import { ValidationSpy } from '@/tests/presentation/mocks'
 
 import faker from 'faker'
@@ -28,5 +30,12 @@ describe('SaveCategoryController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should return 400 Validation if validation fails', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.result = new MissingParamError('id')
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(validationSpy.result))
   })
 })
