ed7b9cdbbafaece04a0a127aaa3a763d0a8e4fd1
vineboneto <vineboneto@gmail.com>
2021-04-10 15:23:14 -0300

    feat: ensure SaveCategoryController call CheckCategoryById with correct value

--- src/presentation/controllers/category/save-category-controller.ts
+++ src/presentation/controllers/category/save-category-controller.ts
@@ -1,12 +1,13 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { badRequest, forbidden } from '@/presentation/helper'
-import { SaveCategory } from '@/domain/usecases'
+import { CheckCategoryById, SaveCategory } from '@/domain/usecases'
 import { AlreadyExistsError } from '@/presentation/errors'
 
 export class SaveCategoryController implements Controller {
   constructor (
     private readonly validation: Validation,
-    private readonly saveCategory: SaveCategory
+    private readonly saveCategory: SaveCategory,
+    private readonly checkCategoryById: CheckCategoryById
   ) {}
 
   async handle (request: SaveCategoryController.Request): Promise<HttpResponse> {
@@ -14,6 +15,7 @@ export class SaveCategoryController implements Controller {
     if (error) {
       return badRequest(error)
     }
+    await this.checkCategoryById.checkById(request.id)
     const isValid = await this.saveCategory.save(request)
     if (!isValid) {
       return forbidden(new AlreadyExistsError(request.name))
