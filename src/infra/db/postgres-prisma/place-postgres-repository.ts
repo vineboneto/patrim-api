19e43dd9f09c0e94f17511d17e63aa6e9995b104
vineboneto <vineboneto@gmail.com>
2021-04-11 15:15:35 -0300

    feat: ensure PlacePostgresRepository return true on update data with userId success

--- src/infra/db/postgres-prisma/place-postgres-repository.ts
+++ src/infra/db/postgres-prisma/place-postgres-repository.ts
@@ -1,14 +1,31 @@
 import { PrismaHelper } from '@/infra/db/postgres-prisma'
-import { AddPlaceRepository } from '@/data/protocols'
+import { AddPlaceRepository, UpdatePlaceRepository } from '@/data/protocols'
 
-export class PlacePostgresRepository implements AddPlaceRepository {
+export class PlacePostgresRepository implements
+  AddPlaceRepository,
+  UpdatePlaceRepository {
   async add (place: AddPlaceRepository.Params): Promise<AddPlaceRepository.Result> {
     const { name, userId } = place
     const prismaClient = await PrismaHelper.getConnection()
     const placeResult = await prismaClient.place.create({
       data: {
         name,
-        userId
+        userId: Number(userId) || undefined
+      }
+    })
+    return placeResult !== null
+  }
+
+  async update (place: UpdatePlaceRepository.Params): Promise<UpdatePlaceRepository.Result> {
+    const { id, name, userId } = place
+    const prismaClient = await PrismaHelper.getConnection()
+    const placeResult = await prismaClient.place.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        name,
+        userId: Number(userId) || undefined
       }
     })
     return placeResult !== null
