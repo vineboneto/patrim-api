3fdc19ba112d2416fcc1da4f261efb87001da208
vineboneto <vineboneto@gmail.com>
2021-04-11 19:44:04 -0300

    fix: change id to string

--- tests/data/usecases/place/db-check-place-by-id.spec.ts
+++ tests/data/usecases/place/db-check-place-by-id.spec.ts
@@ -20,28 +20,28 @@ const makeSut = (): SutTypes => {
 describe('DbCheckPlaceById', () => {
   test('Should call CheckPlaceByIdRepository with correct value', async () => {
     const { sut, checkPlaceByIdRepositorySpy } = makeSut()
-    const id = faker.datatype.number()
+    const id = faker.datatype.number().toString()
     await sut.checkById(id)
     expect(checkPlaceByIdRepositorySpy.id).toBe(id)
   })
 
   test('Should return true if CheckPlaceByIdRepository return true', async () => {
     const { sut } = makeSut()
-    const check = await sut.checkById(faker.datatype.number())
+    const check = await sut.checkById(faker.datatype.number().toString())
     expect(check).toBe(true)
   })
 
   test('Should return false if CheckPlaceByIdRepository return false', async () => {
     const { sut, checkPlaceByIdRepositorySpy } = makeSut()
     checkPlaceByIdRepositorySpy.result = false
-    const check = await sut.checkById(faker.datatype.number())
+    const check = await sut.checkById(faker.datatype.number().toString())
     expect(check).toBe(false)
   })
 
   test('Should throws if CheckPlaceByIdRepository throws', async () => {
     const { sut, checkPlaceByIdRepositorySpy } = makeSut()
     jest.spyOn(checkPlaceByIdRepositorySpy, 'checkById').mockRejectedValueOnce(new Error())
-    const promise = sut.checkById(faker.datatype.number())
+    const promise = sut.checkById(faker.datatype.number().toString())
     await expect(promise).rejects.toThrow()
   })
 })
