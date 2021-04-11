c467e559e90a79caf7c639c3e3c4958aea0014e9
vineboneto <vineboneto@gmail.com>
2021-04-11 15:43:33 -0300

    feat: create PlacePostgreRepository checkByName

--- src/infra/db/postgres-prisma/place-postgres-repository.ts
+++ src/infra/db/postgres-prisma/place-postgres-repository.ts
@@ -1,10 +1,16 @@
 import { PrismaHelper } from '@/infra/db/postgres-prisma'
-import { AddPlaceRepository, UpdatePlaceRepository, CheckPlaceByIdRepository } from '@/data/protocols'
+import {
+  AddPlaceRepository,
+  UpdatePlaceRepository,
+  CheckPlaceByIdRepository,
+  CheckPlaceByNameRepository
+} from '@/data/protocols'
 
 export class PlacePostgresRepository implements
   AddPlaceRepository,
   UpdatePlaceRepository,
-  CheckPlaceByIdRepository {
+  CheckPlaceByIdRepository,
+  CheckPlaceByNameRepository {
   async add (place: AddPlaceRepository.Params): Promise<AddPlaceRepository.Result> {
     const { name, userId } = place
     const prismaClient = await PrismaHelper.getConnection()
@@ -37,6 +43,19 @@ export class PlacePostgresRepository implements
     const placeWithOnlyId = await prismaClient.place.findFirst({
       where: {
         id: Number(id)
+      },
+      select: {
+        id: true
+      }
+    })
+    return placeWithOnlyId !== null
+  }
+
+  async checkByName (name: string): Promise<CheckPlaceByNameRepository.Result> {
+    const prismaClient = await PrismaHelper.getConnection()
+    const placeWithOnlyId = await prismaClient.place.findFirst({
+      where: {
+        name: name
       },
       select: {
         id: true
