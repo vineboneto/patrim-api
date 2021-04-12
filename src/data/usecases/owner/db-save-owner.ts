774332272f6ea6a83a2c3c73bf3f2fa90c7e0774
vineboneto <vineboneto@gmail.com>
2021-04-12 11:52:36 -0300

    feat: ensure DbSaveOwner call CheckSectorByIdRepository with correct value

--- src/data/usecases/owner/db-save-owner.ts
+++ src/data/usecases/owner/db-save-owner.ts
@@ -1,14 +1,16 @@
 import { SaveOwner } from '@/domain/usecases'
-import { AddOwnerRepository, UpdateOwnerRepository } from '@/data/protocols'
+import { AddOwnerRepository, CheckSectorByIdRepository, UpdateOwnerRepository } from '@/data/protocols'
 
 export class DbSaveOwner implements SaveOwner {
   constructor (
     private readonly addOwnerRepository: AddOwnerRepository,
-    private readonly updateOwnerRepository: UpdateOwnerRepository
+    private readonly updateOwnerRepository: UpdateOwnerRepository,
+    private readonly checkSectorByIdRepository: CheckSectorByIdRepository
   ) {}
 
   async save (owner: SaveOwner.Params): Promise<SaveOwner.Model> {
     const { id, name, sectorId } = owner
+    await this.checkSectorByIdRepository.checkById(sectorId)
     if (id) {
       return this.updateOwnerRepository.update({ id, name, sectorId })
     }
