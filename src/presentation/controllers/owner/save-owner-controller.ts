e45093681c9daf7ceeb23bad60e6dc406f9b9814
vineboneto <vineboneto@gmail.com>
2021-04-13 12:01:08 -0300

    feat: ensure SaveOwnerController return 500 if SaveCategory throws

--- src/presentation/controllers/owner/save-owner-controller.ts
+++ src/presentation/controllers/owner/save-owner-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
-import { badRequest, ok } from '@/presentation/helper'
+import { badRequest, ok, serverError } from '@/presentation/helper'
 import { SaveOwner } from '@/domain/usecases'
 import { InvalidParamError } from '@/presentation/errors'
 
@@ -10,15 +10,19 @@ export class SaveOwnerController implements Controller {
   ) {}
 
   async handle (request: SaveOwnerController.Request): Promise<HttpResponse> {
-    const error = this.validation.validate(request)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(request)
+      if (error) {
+        return badRequest(error)
+      }
+      const owner = await this.saveOwner.save(request)
+      if (!owner) {
+        return badRequest(new InvalidParamError('sectorId'))
+      }
+      return ok(owner)
+    } catch (error) {
+      return serverError(error)
     }
-    const owner = await this.saveOwner.save(request)
-    if (!owner) {
-      return badRequest(new InvalidParamError('sectorId'))
-    }
-    return ok(owner)
   }
 }
 
