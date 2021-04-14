d8b9255208d0751081340923da2a15e195e4a11f
vineboneto <vineboneto@gmail.com>
2021-04-14 00:45:30 -0300

    feat: ensure LoadOwnersController return 400 if Validation fails

--- src/presentation/controllers/owner/load-owners-controller.ts
+++ src/presentation/controllers/owner/load-owners-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '@/presentation/helper'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class LoadOwnersController implements Controller {
@@ -6,7 +7,10 @@ export class LoadOwnersController implements Controller {
   ) {}
 
   async handle (request: LoadOwnersController.Request): Promise<HttpResponse> {
-    this.validation.validate(request)
+    const error = this.validation.validate(request)
+    if (error) {
+      return badRequest(error)
+    }
     return null
   }
 }
