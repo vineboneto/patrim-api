df3af0537ea3cf49177a7ffbedc3654f31ed4e08
vineboneto <vineboneto@gmail.com>
2021-04-14 00:56:33 -0300

    feat: ensure LoadOwnersController call LoadOwners with correct values

--- src/presentation/controllers/owner/load-owners-controller.ts
+++ src/presentation/controllers/owner/load-owners-controller.ts
@@ -1,16 +1,20 @@
+import { LoadOwners } from '@/domain/usecases'
 import { badRequest } from '@/presentation/helper'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class LoadOwnersController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly loadOwner: LoadOwners
   ) {}
 
   async handle (request: LoadOwnersController.Request): Promise<HttpResponse> {
-    const error = this.validation.validate(request)
+    const { take, skip } = request
+    const error = this.validation.validate({ take, skip })
     if (error) {
       return badRequest(error)
     }
+    await this.loadOwner.load({ skip: Number(skip), take: Number(take) })
     return null
   }
 }
