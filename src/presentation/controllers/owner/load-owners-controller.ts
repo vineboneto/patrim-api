19ba56b88a759366bf5d6e32abbccd71dc56acc7
vineboneto <vineboneto@gmail.com>
2021-04-14 01:06:02 -0300

    feat: ensure LoadOwnersController throws if LoadOwners throws

--- src/presentation/controllers/owner/load-owners-controller.ts
+++ src/presentation/controllers/owner/load-owners-controller.ts
@@ -1,5 +1,5 @@
 import { LoadOwners } from '@/domain/usecases'
-import { badRequest, noContent, ok } from '@/presentation/helper'
+import { badRequest, noContent, ok, serverError } from '@/presentation/helper'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class LoadOwnersController implements Controller {
@@ -9,13 +9,17 @@ export class LoadOwnersController implements Controller {
   ) {}
 
   async handle (request: LoadOwnersController.Request): Promise<HttpResponse> {
-    const { take, skip } = request
-    const error = this.validation.validate({ take, skip })
-    if (error) {
-      return badRequest(error)
+    try {
+      const { take, skip } = request
+      const error = this.validation.validate({ take, skip })
+      if (error) {
+        return badRequest(error)
+      }
+      const owners = await this.loadOwner.load({ skip: Number(skip), take: Number(take) })
+      return owners.length ? ok(owners) : noContent()
+    } catch (error) {
+      return serverError(error)
     }
-    const owners = await this.loadOwner.load({ skip: Number(skip), take: Number(take) })
-    return owners.length ? ok(owners) : noContent()
   }
 }
 
