bad3ebda67dd11bd8f5c99ab76689a6ae6c24a68
vineboneto <vineboneto@gmail.com>
2021-04-15 12:46:59 -0300

    test: ensure DeleteOwnerController return 403 if loadPatrimonyByOwnerId returns patrimony

--- tests/presentation/controllers/owner/delete-owner-controller.spec.ts
+++ tests/presentation/controllers/owner/delete-owner-controller.spec.ts
@@ -1,5 +1,6 @@
 import { DeleteOwnerController } from '@/presentation/controllers/owner'
-import { ok } from '@/presentation/helper'
+import { LinkedDataError } from '@/presentation/errors'
+import { forbidden, ok } from '@/presentation/helper'
 import { DeleteOwnerSpy, LoadPatrimonyByOwnerIdSpy } from '@/tests/domain/mocks'
 
 import faker from 'faker'
@@ -27,22 +28,31 @@ const makeSut = (): SutTypes => {
 
 describe('DeleteOwnerController', () => {
   test('Should call DeleteOwner with correct value', async () => {
-    const { sut, deleteOwnerSpy } = makeSut()
+    const { sut, deleteOwnerSpy, loadPatrimonyByOwnerIdSpy } = makeSut()
+    loadPatrimonyByOwnerIdSpy.model = null
     const request = mockRequest()
     await sut.handle(request)
     expect(deleteOwnerSpy.params).toEqual({ id: Number(request.id) })
   })
 
-  test('Should return 200 on delete success', async () => {
-    const { sut, deleteOwnerSpy } = makeSut()
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(ok(deleteOwnerSpy.model))
-  })
-
   test('Should call LoadPatrimonyByOwnerId with correct value', async () => {
     const { sut, loadPatrimonyByOwnerIdSpy } = makeSut()
+    loadPatrimonyByOwnerIdSpy.model = null
     const request = mockRequest()
     await sut.handle(request)
     expect(loadPatrimonyByOwnerIdSpy.params).toEqual({ ownerId: Number(request.id) })
+  })
+
+  test('Should return 403 if loadPatrimonyByOwnerId returns patrimony', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(forbidden(new LinkedDataError('patrimonies')))
+  })
+
+  test('Should return 200 on delete success', async () => {
+    const { sut, deleteOwnerSpy, loadPatrimonyByOwnerIdSpy } = makeSut()
+    loadPatrimonyByOwnerIdSpy.model = null
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(ok(deleteOwnerSpy.model))
   })
 })
