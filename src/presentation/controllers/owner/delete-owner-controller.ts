7a96f257b4f7a105efe17275bd091f32dbb2f125
vineboneto <vineboneto@gmail.com>
2021-04-15 12:17:47 -0300

    feat: ensure DeleteOwnerController return 200 on delete success

--- src/presentation/controllers/owner/delete-owner-controller.ts
+++ src/presentation/controllers/owner/delete-owner-controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { DeleteOwner } from '@/domain/usecases'
+import { ok } from '@/presentation/helper'
 
 export class DeleteOwnerController implements Controller {
   constructor (
@@ -7,8 +8,8 @@ export class DeleteOwnerController implements Controller {
   ) {}
 
   async handle (request: DeleteOwnerController.Request): Promise<HttpResponse> {
-    await this.deleteOwner.delete({ id: Number(request.id) })
-    return null
+    const deletedOwner = await this.deleteOwner.delete({ id: Number(request.id) })
+    return ok(deletedOwner)
   }
 }
 
