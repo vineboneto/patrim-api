80a61ab02f076631202299c523a25712e703c2ef
vineboneto <vineboneto@gmail.com>
2021-04-17 00:43:40 -0300

    feat: ensure PlacePostgresRepository return true if id exists

--- src/infra/db/postgres-prisma/place-postgres-repository.ts
+++ src/infra/db/postgres-prisma/place-postgres-repository.ts
@@ -1,5 +1,11 @@
 import { PrismaHelper } from '@/infra/db/postgres-prisma'
-import { AddPlaceRepository, CheckPlaceByNameRepository, UpdatePlaceRepository } from '@/data/protocols'
+import {
+  AddPlaceRepository,
+  CheckPlaceByIdRepository,
+  CheckPlaceByNameRepository,
+  CheckSectorByIdRepository,
+  UpdatePlaceRepository
+} from '@/data/protocols'
 
 export class PlacePostgresRepository implements
 AddPlaceRepository, UpdatePlaceRepository, CheckPlaceByNameRepository {
@@ -30,6 +36,20 @@ AddPlaceRepository, UpdatePlaceRepository, CheckPlaceByNameRepository {
     const placeModel = await prismaClient.place.findFirst({
       where: {
         name
+      },
+      select: {
+        id: true
+      }
+    })
+    return placeModel !== null
+  }
+
+  async checkById (params: CheckPlaceByIdRepository.Params): Promise<CheckSectorByIdRepository.Result> {
+    const prismaClient = PrismaHelper.getConnection()
+    const { id } = params
+    const placeModel = await prismaClient.place.findFirst({
+      where: {
+        id
       },
       select: {
         id: true
