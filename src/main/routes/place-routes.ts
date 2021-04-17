96284d9795ee5aa96c34fbc723b404124f2c99b4
vineboneto <vineboneto@gmail.com>
2021-04-17 04:12:43 -0300

    feat: create router to delete place

--- src/main/routes/place-routes.ts
+++ src/main/routes/place-routes.ts
@@ -1,4 +1,4 @@
-import { makeSavePlaceController } from '@/main/factories/controllers'
+import { makeDeletePlaceController, makeSavePlaceController } from '@/main/factories/controllers'
 import { adaptRoute } from '@/main/adapters'
 import { auth, checkPlaceId as checkId } from '@/main/middlewares'
 
@@ -7,4 +7,5 @@ import { Router } from 'express'
 export default (router: Router): void => {
   router.post('/places', auth, adaptRoute(makeSavePlaceController()))
   router.put('/places/:id', auth, checkId, adaptRoute(makeSavePlaceController()))
+  router.delete('/places/:id', auth, checkId, adaptRoute(makeDeletePlaceController()))
 }
