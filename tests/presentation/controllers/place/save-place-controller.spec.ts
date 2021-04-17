9625e79894dba30160a14a5206488bae5a3b8d07
vineboneto <vineboneto@gmail.com>
2021-04-17 00:58:57 -0300

    test: ensure SavePlaceController return 400 if Validation fails

--- tests/presentation/controllers/place/save-place-controller.spec.ts
+++ tests/presentation/controllers/place/save-place-controller.spec.ts
@@ -1,4 +1,5 @@
 import { SavePlaceController } from '@/presentation/controllers'
+import { badRequest } from '@/presentation/helper'
 import { ValidationSpy } from '@/tests/presentation/mocks'
 
 import faker from 'faker'
@@ -28,5 +29,12 @@ describe('SavePlaceController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.result = new Error()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
