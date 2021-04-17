51b0cfe0d16df0c143e9b35b55de8672ecafdaf6
vineboneto <vineboneto@gmail.com>
2021-04-17 02:01:11 -0300

    feat: create router do update place

--- src/main/routes/place-routes.ts
+++ src/main/routes/place-routes.ts
@@ -1,9 +1,10 @@
 import { makeSavePlaceController } from '@/main/factories/controllers'
 import { adaptRoute } from '@/main/adapters'
-import { auth } from '@/main/middlewares'
+import { auth, checkPlaceId as checkId } from '@/main/middlewares'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/places', auth, adaptRoute(makeSavePlaceController()))
+  router.put('/places/:id', auth, checkId, adaptRoute(makeSavePlaceController()))
 }
