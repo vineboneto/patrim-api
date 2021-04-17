38559444208bc241838f6e5cfb789ccab4e9da7b
vineboneto <vineboneto@gmail.com>
2021-04-16 23:58:19 -0300

    feat: ensure PlacePostgresRepository returns place on update success

--- src/infra/db/postgres-prisma/place-postgres-repository.ts
+++ src/infra/db/postgres-prisma/place-postgres-repository.ts
@@ -1,11 +1,25 @@
 import { PrismaHelper } from '@/infra/db/postgres-prisma'
-import { AddPlaceRepository } from '@/data/protocols'
+import { AddPlaceRepository, UpdatePlaceRepository } from '@/data/protocols'
 
-export class PlacePostgresRepository implements AddPlaceRepository {
+export class PlacePostgresRepository implements AddPlaceRepository, UpdatePlaceRepository {
   async add (params: AddPlaceRepository.Params): Promise<AddPlaceRepository.Model> {
     const prismaClient = PrismaHelper.getConnection()
     const placeModel = await prismaClient.place.create({
       data: params
+    })
+    return placeModel
+  }
+
+  async update (params: UpdatePlaceRepository.Params): Promise<UpdatePlaceRepository.Model> {
+    const prismaClient = PrismaHelper.getConnection()
+    const { id, name } = params
+    const placeModel = await prismaClient.place.update({
+      data: {
+        name
+      },
+      where: {
+        id: Number(id)
+      }
     })
     return placeModel
   }
