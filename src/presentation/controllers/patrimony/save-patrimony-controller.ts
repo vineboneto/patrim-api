a4037efeaa7fa2f544ad52034508bee80041252f
vineboneto <vineboneto@gmail.com>
2021-04-19 01:39:34 -0300

    feat: ensure SavePatrimonyController call CheckCategory with correct values

--- src/presentation/controllers/patrimony/save-patrimony-controller.ts
+++ src/presentation/controllers/patrimony/save-patrimony-controller.ts
@@ -1,9 +1,11 @@
-import { badRequest } from '@/presentation/helper'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
+import { badRequest } from '@/presentation/helper'
+import { CheckCategoryById } from '@/domain/usecases'
 
 export class SavePatrimonyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly checkCategoryById: CheckCategoryById
   ) {}
 
   async handle (request: SavePatrimonyController.Request): Promise<HttpResponse> {
@@ -11,6 +13,7 @@ export class SavePatrimonyController implements Controller {
     if (error) {
       return badRequest(error)
     }
+    await this.checkCategoryById.checkById({ id: request.categoryId })
     return null
   }
 }
