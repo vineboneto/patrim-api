90949ee65767460c081840c602cf1eb83d406566
vineboneto <vineboneto@gmail.com>
2021-04-19 03:01:46 -0300

    feat: ensure CheckExistIdOwner return InvalidParamError if CheckOwnerById returns false

--- src/validation/checks/check-exist-id-owner.ts
+++ src/validation/checks/check-exist-id-owner.ts
@@ -1,5 +1,6 @@
 import { CheckExist } from '@/presentation/protocols'
 import { CheckOwnerById } from '@/domain/usecases'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class CheckExistIdOwner implements CheckExist {
   constructor (
@@ -8,7 +9,10 @@ export class CheckExistIdOwner implements CheckExist {
   ) {}
 
   async check (input: object): Promise<Error> {
-    await this.checkOwnerById.checkById({ id: input[this.fieldName] })
+    const isValid = await this.checkOwnerById.checkById({ id: input[this.fieldName] })
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName)
+    }
     return null
   }
 }
