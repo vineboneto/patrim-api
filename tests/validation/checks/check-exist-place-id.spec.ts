3dac0f4d3f7d42cdfb3206925151dda3d0d2f496
vineboneto <vineboneto@gmail.com>
2021-04-19 03:14:08 -0300

    test: create CheckExistCategoryId

--- tests/validation/checks/check-exist-place-id.spec.ts
+++ tests/validation/checks/check-exist-place-id.spec.ts
@@ -1,41 +1,41 @@
-import { CheckExistPlaceId } from '@/validation/checks'
-import { CheckPlaceByIdSpy } from '@/tests/domain/mocks'
+import { CheckExistCategoryId } from '@/validation/checks'
+import { CheckCategoryByIdSpy } from '@/tests/domain/mocks'
 import { InvalidParamError } from '@/presentation/errors'
 
 import faker from 'faker'
 
 type SutTypes = {
-  sut: CheckExistPlaceId
-  checkPlaceByIdSpy: CheckPlaceByIdSpy
+  sut: CheckExistCategoryId
+  checkCategoryByIdSpy: CheckCategoryByIdSpy
 }
 
 const makeSut = (): SutTypes => {
-  const checkPlaceByIdSpy = new CheckPlaceByIdSpy()
-  const sut = new CheckExistPlaceId(checkPlaceByIdSpy, 'placeId')
+  const checkCategoryByIdSpy = new CheckCategoryByIdSpy()
+  const sut = new CheckExistCategoryId(checkCategoryByIdSpy, 'categoryId')
   return {
     sut,
-    checkPlaceByIdSpy
+    checkCategoryByIdSpy
   }
 }
 
-describe('CheckExistPlaceId', () => {
-  test('Should call CheckPlaceById with correct values', async () => {
-    const { sut, checkPlaceByIdSpy } = makeSut()
+describe('CheckExistCategoryId', () => {
+  test('Should call CheckCategoryById with correct values', async () => {
+    const { sut, checkCategoryByIdSpy } = makeSut()
     const id = faker.datatype.number()
-    await sut.check({ placeId: id })
-    expect(checkPlaceByIdSpy.params).toEqual({ id })
+    await sut.check({ categoryId: id })
+    expect(checkCategoryByIdSpy.params).toEqual({ id })
   })
 
-  test('Should return InvalidParamError if CheckPlaceById returns false', async () => {
-    const { sut, checkPlaceByIdSpy } = makeSut()
-    checkPlaceByIdSpy.result = false
-    const error = await sut.check({ placeId: faker.datatype.number() })
-    expect(error).toEqual(new InvalidParamError('placeId'))
+  test('Should return InvalidParamError if CheckCategoryById returns false', async () => {
+    const { sut, checkCategoryByIdSpy } = makeSut()
+    checkCategoryByIdSpy.result = false
+    const error = await sut.check({ categoryId: faker.datatype.number() })
+    expect(error).toEqual(new InvalidParamError('categoryId'))
   })
 
-  test('Should return null if CheckPlaceById returns true', async () => {
+  test('Should return null if CheckCategoryById returns true', async () => {
     const { sut } = makeSut()
-    const error = await sut.check({ placeId: faker.datatype.number() })
+    const error = await sut.check({ categoryId: faker.datatype.number() })
     expect(error).toEqual(null)
   })
 })
