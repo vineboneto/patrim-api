f36cad9545454a01d8ff599b55812186707b579d
vineboneto <vineboneto@gmail.com>
2021-04-19 02:37:26 -0300

    feat: ensure SavePatrimonyController return 500 if SavePatrimony throws

--- src/presentation/controllers/patrimony/save-patrimony-controller.ts
+++ src/presentation/controllers/patrimony/save-patrimony-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
-import { badRequest, forbidden, ok, unprocessableEntity } from '@/presentation/helper'
+import { badRequest, forbidden, ok, serverError, unprocessableEntity } from '@/presentation/helper'
 import { CheckCategoryById, CheckOwnerById, CheckPlaceById, SavePatrimony } from '@/domain/usecases'
 import { AlreadyExistsError, InvalidParamError } from '@/presentation/errors'
 
@@ -13,28 +13,32 @@ export class SavePatrimonyController implements Controller {
   ) {}
 
   async handle (request: SavePatrimonyController.Request): Promise<HttpResponse> {
-    const error = this.validation.validate(request)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(request)
+      if (error) {
+        return badRequest(error)
+      }
+      let isValid: boolean
+      isValid = await this.checkCategoryById.checkById({ id: request.categoryId })
+      if (!isValid) {
+        return forbidden(new InvalidParamError('categoryId'))
+      }
+      isValid = await this.checkPlaceById.checkById({ id: request.placeId })
+      if (!isValid) {
+        return forbidden(new InvalidParamError('placeId'))
+      }
+      isValid = await this.checkOwnerById.checkById({ id: request.ownerId })
+      if (!isValid) {
+        return forbidden(new InvalidParamError('ownerId'))
+      }
+      const patrimonyModel = await this.savePatrimony.save(request)
+      if (!patrimonyModel) {
+        return unprocessableEntity(new AlreadyExistsError(request.number))
+      }
+      return ok(patrimonyModel)
+    } catch (error) {
+      return serverError(error)
     }
-    let isValid: boolean
-    isValid = await this.checkCategoryById.checkById({ id: request.categoryId })
-    if (!isValid) {
-      return forbidden(new InvalidParamError('categoryId'))
-    }
-    isValid = await this.checkPlaceById.checkById({ id: request.placeId })
-    if (!isValid) {
-      return forbidden(new InvalidParamError('placeId'))
-    }
-    isValid = await this.checkOwnerById.checkById({ id: request.ownerId })
-    if (!isValid) {
-      return forbidden(new InvalidParamError('ownerId'))
-    }
-    const patrimonyModel = await this.savePatrimony.save(request)
-    if (!patrimonyModel) {
-      return unprocessableEntity(new AlreadyExistsError(request.number))
-    }
-    return ok(patrimonyModel)
   }
 }
 
