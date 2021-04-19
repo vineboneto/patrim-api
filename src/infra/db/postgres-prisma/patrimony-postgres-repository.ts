bbbcec9fd2457543e47ddef98e957a62f0d9438e
vineboneto <vineboneto@gmail.com>
2021-04-19 00:05:03 -0300

    feat: ensure PatrimonyPostgresRepository return patrimony on add success

--- src/infra/db/postgres-prisma/patrimony-postgres-repository.ts
+++ src/infra/db/postgres-prisma/patrimony-postgres-repository.ts
@@ -3,14 +3,39 @@ import {
   CheckPatrimonyByOwnerIdRepository,
   LoadPatrimonyByOwnerIdRepository,
   CheckPatrimonyByCategoryIdRepository,
-  CheckPatrimonyByPlaceIdRepository
+  CheckPatrimonyByPlaceIdRepository,
+  AddPatrimonyRepository
 } from '@/data/protocols'
+import { Category, Owner, Patrimony, Place } from '@prisma/client'
+
+type PatrimonyPrisma = Patrimony & { Category: Category, Owner: Owner, Place: Place }
 
 export class PatrimonyPostgresRepository implements
+  AddPatrimonyRepository,
   LoadPatrimonyByOwnerIdRepository,
   CheckPatrimonyByOwnerIdRepository,
   CheckPatrimonyByCategoryIdRepository,
   CheckPatrimonyByPlaceIdRepository {
+  async add (params: AddPatrimonyRepository.Params): Promise<AddPatrimonyRepository.Model> {
+    const prismaClient = PrismaHelper.getConnection()
+    const patrimony = await prismaClient.patrimony.create({
+      data: {
+        number: params.number,
+        brand: params.brand,
+        description: params?.description,
+        ownerId: params.ownerId,
+        placeId: params.placeId,
+        categoryId: params.categoryId
+      },
+      include: {
+        Category: true,
+        Owner: true,
+        Place: true
+      }
+    })
+    return this.adapt(patrimony)
+  }
+
   async loadByOwnerId (params: LoadPatrimonyByOwnerIdRepository.Params):
   Promise<LoadPatrimonyByOwnerIdRepository.Model> {
     const prismaClient = PrismaHelper.getConnection()
@@ -69,5 +94,30 @@ export class PatrimonyPostgresRepository implements
       }
     })
     return patrimony !== null
+  }
+
+  private adapt (patrimony: PatrimonyPrisma): AddPatrimonyRepository.Model {
+    return {
+      id: patrimony.id,
+      number: patrimony.number,
+      description: patrimony.description,
+      brand: patrimony.brand,
+      category: {
+        id: patrimony.Category.id,
+        name: patrimony.Category.name
+      },
+      owner: {
+        id: patrimony.Owner.id,
+        name: patrimony.Owner.name,
+        sector: {
+          id: patrimony.Owner.id,
+          name: patrimony.Owner.name
+        }
+      },
+      place: {
+        id: patrimony.Owner.id,
+        name: patrimony.Owner.name
+      }
+    }
   }
 }
