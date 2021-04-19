55289e48fc77b38c3761361aaca333f4e922a0fe
vineboneto <vineboneto@gmail.com>
2021-04-19 03:03:02 -0300

    test: ensure CheckExistIdOwner return InvalidParamError if CheckOwnerById returns false

--- tests/validation/checks/check-exist-id-owner.spec.ts
+++ tests/validation/checks/check-exist-id-owner.spec.ts
@@ -2,6 +2,7 @@ import { CheckExistIdOwner } from '@/validation/checks'
 import { CheckOwnerByIdSpy } from '@/tests/domain/mocks'
 
 import faker from 'faker'
+import { InvalidParamError } from '@/presentation/errors'
 
 type SutTypes = {
   sut: CheckExistIdOwner
@@ -23,5 +24,12 @@ describe('CheckExistIdOwner', () => {
     const id = faker.datatype.number()
     await sut.check({ ownerId: id })
     expect(checkOwnerByIdSpy.params).toEqual({ id })
+  })
+
+  test('Should return InvalidParamError if CheckOwnerById returns false', async () => {
+    const { sut, checkOwnerByIdSpy } = makeSut()
+    checkOwnerByIdSpy.result = false
+    const error = await sut.check({ ownerId: faker.datatype.number() })
+    expect(error).toEqual(new InvalidParamError('ownerId'))
   })
 })
