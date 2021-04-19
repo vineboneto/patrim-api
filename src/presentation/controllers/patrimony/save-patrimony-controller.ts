604759951f3a619c16366cd391b26847f2759c34
vineboneto <vineboneto@gmail.com>
2021-04-19 01:49:10 -0300

    feat: ensure SavePatrimonyController call CheckPlaceById with correct values

--- src/presentation/controllers/patrimony/save-patrimony-controller.ts
+++ src/presentation/controllers/patrimony/save-patrimony-controller.ts
@@ -1,12 +1,13 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { badRequest, forbidden } from '@/presentation/helper'
-import { CheckCategoryById } from '@/domain/usecases'
+import { CheckCategoryById, CheckPlaceById } from '@/domain/usecases'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SavePatrimonyController implements Controller {
   constructor (
     private readonly validation: Validation,
-    private readonly checkCategoryById: CheckCategoryById
+    private readonly checkCategoryById: CheckCategoryById,
+    private readonly checkPlaceById: CheckPlaceById
   ) {}
 
   async handle (request: SavePatrimonyController.Request): Promise<HttpResponse> {
@@ -18,6 +19,7 @@ export class SavePatrimonyController implements Controller {
     if (!isValid) {
       return forbidden(new InvalidParamError('categoryId'))
     }
+    await this.checkPlaceById.checkById({ id: request.placeId })
     return null
   }
 }
