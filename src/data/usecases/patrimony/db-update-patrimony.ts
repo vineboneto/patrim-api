ecba0080b104fbdb318ba9625289d0af4b150e2a
vineboneto <vineboneto@gmail.com>
2021-04-20 12:18:22 -0300

    feat: ensure DbUpdatePatrimony call CheckPatrimonyByNumberRepository with correct values

--- src/data/usecases/patrimony/db-update-patrimony.ts
+++ src/data/usecases/patrimony/db-update-patrimony.ts
@@ -1,16 +1,23 @@
 import { UpdatePatrimony } from '@/domain/usecases'
-import { LoadPatrimonyNumberByIdRepository, UpdatePatrimonyRepository } from '@/data/protocols'
+import {
+  CheckPatrimonyByNumberRepository,
+  LoadPatrimonyNumberByIdRepository,
+  UpdatePatrimonyRepository
+} from '@/data/protocols'
 
 export class DbUpdatePatrimony implements UpdatePatrimony {
   constructor (
     private readonly updatePatrimonyRepository: UpdatePatrimonyRepository,
-    private readonly loadPatrimonyNumberByIdRepository: LoadPatrimonyNumberByIdRepository
+    private readonly loadPatrimonyNumberByIdRepository: LoadPatrimonyNumberByIdRepository,
+    private readonly checkPatrimonyByNumberRepository: CheckPatrimonyByNumberRepository
   ) {}
 
   async update (params: UpdatePatrimony.Params): Promise<UpdatePatrimony.Model> {
     const { number } = await this.loadPatrimonyNumberByIdRepository.loadNumberById(params.id)
     if (number === params.number) {
       return this.updatePatrimonyRepository.update(params)
+    } else {
+      await this.checkPatrimonyByNumberRepository.checkByNumber(params.number)
     }
     return null
   }
