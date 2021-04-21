9f093e7545a0f34cea16026b217891a11f575c54
vineboneto <vineboneto@gmail.com>
2021-04-20 21:20:54 -0300

    feat: create usecase UpdatePatrimony

---
+++ src/domain/usecases/category/update-category.ts
@@ -0,0 +1,14 @@
+import { CategoryModel } from '@/domain/models'
+
+export interface UpdateCategory {
+  update (params: UpdateCategory.Params): Promise<UpdateCategory.Model>
+}
+
+export namespace UpdateCategory {
+  export type Params = {
+    id: number
+    name: string
+  }
+
+  export type Model = CategoryModel
+}
