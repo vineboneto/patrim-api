323f723269798b005577eef3249072928731a002
vineboneto <vineboneto@gmail.com>
2021-04-20 21:30:19 -0300

    feat: ensure DbUpdateCategory call UpdateCategoryRepository with correct value

---
+++ src/data/usecases/category/db-update-category.ts
@@ -0,0 +1,14 @@
+import { UpdateCategory } from '@/domain/usecases'
+import { UpdateCategoryRepository } from '@/data/protocols'
+
+export class DbUpdateCategory implements UpdateCategory {
+  constructor (
+    private readonly updateCategoryRepository: UpdateCategoryRepository
+  ) {}
+
+  async update (category: UpdateCategory.Params): Promise<UpdateCategory.Model> {
+    const { id, name } = category
+    await this.updateCategoryRepository.update({ id, name })
+    return null
+  }
+}
