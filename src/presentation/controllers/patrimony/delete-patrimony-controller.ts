cf35d12eff8fa4da601df3ca1ab5096df5d9ddaa
vineboneto <vineboneto@gmail.com>
2021-04-21 13:43:07 -0300

    feat: create DeletePatrimonyController

---
+++ src/presentation/controllers/patrimony/delete-patrimony-controller.ts
@@ -0,0 +1,34 @@
+import { CheckExist, Controller, HttpResponse, Validation } from '@/presentation/protocols'
+import { badRequest, forbidden, ok, serverError } from '@/presentation/helper'
+import { DeletePatrimony } from '@/domain/usecases'
+
+export class DeletePatrimonyController implements Controller {
+  constructor (
+    private readonly deletePatrimony: DeletePatrimony,
+    private readonly checkExist: CheckExist,
+    private readonly validation: Validation
+  ) {}
+
+  async handle (request: DeletePatrimonyController.Request): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate(request)
+      if (error) {
+        return badRequest(error)
+      }
+      const existError = await this.checkExist.check(request)
+      if (existError) {
+        return forbidden(existError)
+      }
+      const patrimonyDeleted = await this.deletePatrimony.delete(request)
+      return ok(patrimonyDeleted)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
+
+export namespace DeletePatrimonyController {
+  export type Request = {
+    id: number
+  }
+}
--- src/presentation/controllers/patrimony/index.ts
+++ src/presentation/controllers/patrimony/index.ts
@@ -1,2 +1,3 @@
 export * from './add-patrimony-controller'
 export * from './update-patrimony-controller'
+export * from './delete-patrimony-controller'
