cb907329bc9ab8df92d3269fbf16372e4dd8ad32
vineboneto <vineboneto@gmail.com>
2021-04-20 21:53:58 -0300

    feat: ensure DbUpdateCategory call CheckCategoryByNameRepository with correct value

--- src/data/usecases/category/db-update-category.ts
+++ src/data/usecases/category/db-update-category.ts
@@ -1,14 +1,20 @@
 import { UpdateCategory } from '@/domain/usecases'
-import { LoadCategoryNameByIdRepository, UpdateCategoryRepository } from '@/data/protocols'
+import {
+  CheckCategoryByNameRepository,
+  LoadCategoryNameByIdRepository,
+  UpdateCategoryRepository
+} from '@/data/protocols'
 
 export class DbUpdateCategory implements UpdateCategory {
   constructor (
     private readonly updateCategoryRepository: UpdateCategoryRepository,
-    private readonly loadCategoryNameByIdRepository: LoadCategoryNameByIdRepository
+    private readonly loadCategoryNameByIdRepository: LoadCategoryNameByIdRepository,
+    private readonly checkCategoryByNameRepository: CheckCategoryByNameRepository
   ) {}
 
   async update (params: UpdateCategory.Params): Promise<UpdateCategory.Model> {
     await this.loadCategoryNameByIdRepository.loadNameById(params.id)
+    await this.checkCategoryByNameRepository.checkByName(params.name)
     return this.updateCategoryRepository.update(params)
   }
 }
