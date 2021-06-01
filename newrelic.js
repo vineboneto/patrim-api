5e7bdf06281e44b26c85177141b83b09daa7129f
vineboneto <vineboneto@gmail.com>
2021-05-31 23:25:32 -0300

    chore: add new relic

---
+++ newrelic.js
@@ -0,0 +1,7 @@
+exports.config = {
+  app_name: ['patrim-api'],
+  license_key: 'd4fe3dcaed33399592a918cbe4ac7e5a4b21NRAL',
+  logging: {
+    level: 'info'
+  }
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,5 @@
 import 'module-alias/register'
+import 'newrelic'
 import env from '@/main/config/env'
 import app from '@/main/config/app'
 import { PrismaHelper } from '@/infra/db/postgres-prisma'
