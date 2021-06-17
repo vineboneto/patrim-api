9d61d0914c6fbb539586b9298a7e3c7f82ce1bc2
vineboneto <vineboneto@gmail.com>
2021-06-17 00:17:35 -0300

    test: ensure CheckAccessDataDecorator return the same result of the controller

--- tests/main/decorators/check-access-data-decorator.spec.ts
+++ tests/main/decorators/check-access-data-decorator.spec.ts
@@ -50,4 +50,10 @@ describe('CheckAccessDataDecorator', () => {
     await sut.handle(request)
     expect(controllerSpy.request).toEqual(request)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut, controllerSpy } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(controllerSpy.httpResponse)
+  })
 })
