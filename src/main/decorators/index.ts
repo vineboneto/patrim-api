dad994103f061927b7f49ab0e22af03875476d2b
vineboneto <vineboneto@gmail.com>
2021-06-17 12:47:04 -0300

    refactor: remove unecessary code

--- src/main/decorators/index.ts
+++ src/main/decorators/index.ts
@@ -1,3 +1,2 @@
 export * from './log-controller-decorator'
 export * from './log-update-patrimony-decorator'
-export * from './check-access-data-decorator'
--- src/main/factories/decorators/index.ts
+++ src/main/factories/decorators/index.ts
@@ -1,3 +1,2 @@
 export * from './log-controller-decorator-factory'
 export * from './log-update-patrimony-decorator-factory'
-export * from './check-access-data-decorator-factory'
